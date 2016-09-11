0e2591faed674c60046860a81914463e4f82d4d4
琨瑜 <yiyan.lyy@alibaba-inc.com>
2016-09-11 18:33:07 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # run shadowsocks server
-shadowsocks-server -server_port 8888 -method aes-256-cfb -password ${PASSWORD} &
+shadowsocks-server -p 8888 -m aes-256-cfb -k ${PASSWORD} &
 # run kcp server
 server -t 127.0.0.1:8888 -l :29900 --mode ${MODE} --key ${PASSWORD} 
